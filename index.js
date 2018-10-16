a5cadcbafba01668aade512cf62af8ff5e93646a
Joseph Jin <jerkjoe61@gmail.com>
2018-10-15 22:33:21 -0700

    node server

---
+++ index.js
@@ -0,0 +1,42 @@
+/*
+  http --> using ES5
+*/
+// ES5
+var http = require('http')
+var fs = require('fs')
+
+// 创建 Server
+var myServer = http.createServer(function(request, response) {
+  console.log('I am server!')
+  console.log(request.url)
+  // check request
+  switch (request.url) {
+    case '/1.html':
+      response.write('1.html')
+      break;
+    default:
+      response.write('404')
+      break;
+  }
+
+  
+  
+  // end sending response
+  response.end()
+})
+
+// 监听
+myServer.listen(8082) // $param --> port number
+
+// 返回 response
+
+
+
+
+
+
+// read file
+/* 
+fs.readFile(filename, callback() {})
+
+*/
---
+++ server.js
@@ -0,0 +1,63 @@
+var http = require('http')
+var fs = require('fs')
+var queryString = require('querystring')
+var urlLib = require('url')
+
+var server = http.createServer(function(req, res) {
+  var str = ''
+  req.on('data', function(data) {
+    str += data
+  })
+  req.on('end', function() {
+    var obj = urlLib.parse(req.url, true)
+    const url = obj.name
+    const GET = obj.query
+    const POST = queryString.parse(str)
+
+    var users = {
+      user1: 'password1'
+    }
+    if (url === './user') {
+      switch (GET.act) {
+        case 'reg':
+          if (users[GET.user]) {
+            res.write('{"ok": false, "msg": username has been taken!}')
+          } else {
+            // store username and password to json
+            users[GET.user] = GET.pass
+            res.write('{"ok": true, "msg": Registration Success!}')
+          }
+          break
+        case 'login':
+          if (!users[GET.name]) {
+            res.write('{"ok": false, "msg": User not exist!}')
+          } else if (users[GET.user] != GET.pass) {
+            res.write('{"ok": false, "msg": Wrong password!}')
+          }
+          else {
+            res.write('{"ok": true, "msg": Login Success!}')
+          }
+          break
+        default:
+          res.write('{"ok": false, "msg": "unknown action!"}')
+          break
+      }
+      res.end()
+    } else {
+      // read
+      var fileName = './www' + url
+      fs.readFile(fileName, function(err, data) {
+        if (err) {
+          res.write('404')
+        } else {
+          res.write(data)
+        }
+
+        res.end()
+      })
+    }
+  })
+})
+
+
+server.listen(8082)
